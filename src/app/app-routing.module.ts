4ad0cb904b257467b86cbdb2c2f8c84956531fe0
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-09-29 15:08:01 -0300

    Criando componente de Busca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,34 @@ import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { authGuard } from './core/guards/auth.guard';
+import { BuscaComponent } from './pages/busca/busca.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'cadastro',
-    component: CadastroComponent
+    component: CadastroComponent,
   },
   {
     path: 'perfil',
     component: PerfilComponent,
-    canActivate: [authGuard]
-  }
+    canActivate: [authGuard],
+  },
+  {
+    path: 'busca',
+    component: BuscaComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
