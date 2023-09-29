c6f651b2646bab15a49c8faabe9e4dffbb2e9c4a
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-09-29 16:14:08 -0300

    Navegando com os dados

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 
 @Component({
   selector: 'app-form-busca',
   templateUrl: './form-busca.component.html',
-  styleUrls: ['./form-busca.component.scss']
+  styleUrls: ['./form-busca.component.scss'],
 })
 export class FormBuscaComponent {
-  constructor( 
-    public formBuscaService : FormBuscaService) {}
+  @Output() realizarBusca = new EventEmitter();
+  constructor(public formBuscaService: FormBuscaService) {}
 
-  buscar () {
-    console.log(this.formBuscaService.formBusca.value)
+  buscar() {
+    const formBuscaValue = this.formBuscaService.formBusca.value;
+    this.realizarBusca.emit(formBuscaValue);
   }
 }
